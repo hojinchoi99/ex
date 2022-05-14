0e43c29cacc1a78c7d963ac240c59e2296df6c11
Woony <clover7kso@naver.com>
2022-05-14 22:07:26 +0900

    feat: refactoring code

---
+++ src/util/menuList.js
@@ -0,0 +1,27 @@
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GroupsIcon from "@mui/icons-material/Groups";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import StadiumIcon from "@mui/icons-material/Stadium";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+const menuList = [
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "플랩 풋볼 소개1",
+    icon: () => <SportsSoccerIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "플랩 풋볼 소개2",
+    icon: () => <SportsSoccerIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "플랩 풋볼 소개3",
+    icon: () => <SportsSoccerIcon />,
+  },
+];
+
+export default menuList;
