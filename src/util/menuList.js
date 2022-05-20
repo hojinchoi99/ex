e10716f655360610600aa2d543413b021929d415
CHOI HOJIN <hojinch99@gmail.com>
2022-05-20 18:37:00 +0900

    sixth commit

--- src/util/menuList.js
+++ src/util/menuList.js
@@ -6,22 +6,47 @@ import CampaignIcon from "@mui/icons-material/Campaign";
 import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
 import StadiumIcon from "@mui/icons-material/Stadium";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-const menuList = [
+const MenuList = [
   {
     url: "https://www.plabfootball.com/about/",
-    name: "플랩 풋볼 소개1",
+    name: "플랩풋볼 소개",
     icon: () => <SportsSoccerIcon />,
   },
   {
     url: "https://www.plabfootball.com/about/",
-    name: "플랩 풋볼 소개2",
-    icon: () => <SportsSoccerIcon />,
+    name: "매니저 지원",
+    icon: () => <AlternateEmailIcon />,
   },
   {
     url: "https://www.plabfootball.com/about/",
-    name: "플랩 풋볼 소개3",
-    icon: () => <SportsSoccerIcon />,
+    name: "구장 제휴",
+    icon: () => <StadiumIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "공지사항",
+    icon: () => <CampaignIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "자주 묻는 질문",
+    icon: () => <HelpOutlineIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "페이스북",
+    icon: () => <FacebookIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "페이스북 그룹",
+    icon: () => <GroupsIcon />,
+  },
+  {
+    url: "https://www.plabfootball.com/about/",
+    name: "인스타그램",
+    icon: () => <InstagramIcon />,
   },
 ];
 
-export default menuList;
+export default MenuList;
